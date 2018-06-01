f5b181ebb1ed8a4b526b6fd0a76ff417a0fcf9c3
Fred Proctor <frederick.proctor@nist.gov>
2018-06-01 17:06:41 -0400

    threaded read and write

--- src/smartmotor_md.c
+++ src/smartmotor_md.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include "ulapi.h"
 
-static void task_code(void *serial)
+/*
+  Time is in units of servo samples, where 128 samples is 32 ms.
+
+  WHEREWASI - you are sending pos-time pairs in write_code, but still
+  getting the 249 32 0 0 0 response indicating too much data (?)
+*/
+
+static void read_code(void *serial)
 {
   enum {BUFFERLEN = 80};
   char buffer[BUFFERLEN];
   int nchars;
   int t;
-  int ival;
+  unsigned char ival;
 
   for (;;) {
     nchars = ulapi_serial_read(serial, buffer, sizeof(buffer)-1);
@@ -16,8 +23,7 @@ static void task_code(void *serial)
     if (nchars == 0) continue;
     for (t = 0; t < nchars; t++) {
       ival = buffer[t];
-      printf("%X ", ival);
-      printf("\n");
+      printf("%X (%u)\n", ival, ival);
     }
   }
 
@@ -26,7 +32,43 @@ static void task_code(void *serial)
   return;
 }
 
-/* syntax: serial <port> <baud> */
+static ulapi_mutex_struct mutex;
+
+enum {PERIOD_NSECS = 32000000};
+
+static void write_code(void *serial)
+{
+  char position_buffer[] = {250, 0, 0, 0, 0, 13};
+  char time_buffer[] = {251, 0, 0, 0, 128, 13}; /* 128 samples = 32 msec */
+
+  ulapi_mutex_take(&mutex);
+  ulapi_serial_write(serial, position_buffer, sizeof(position_buffer));
+  time_buffer[4] = 0;
+  ulapi_serial_write(serial, time_buffer, sizeof(time_buffer));
+  ulapi_mutex_give(&mutex);
+  ulapi_wait(PERIOD_NSECS);
+
+  ulapi_mutex_take(&mutex);
+  ulapi_serial_write(serial, position_buffer, sizeof(position_buffer));
+  time_buffer[4] = 128;
+  ulapi_serial_write(serial, time_buffer, sizeof(time_buffer));
+  ulapi_mutex_give(&mutex);
+  ulapi_wait(PERIOD_NSECS);
+  
+  for (;;) {
+    ulapi_mutex_take(&mutex);
+    ulapi_serial_write(serial, position_buffer, sizeof(position_buffer));
+    ulapi_serial_write(serial, time_buffer, sizeof(time_buffer));
+    ulapi_mutex_give(&mutex);
+    ulapi_wait(PERIOD_NSECS);
+  }
+
+  printf("done\n");
+
+  return;
+}
+
+/* syntax: smartmotor_md <port> <baud> */
 
 int main(int argc, char * argv[])
 {
@@ -35,10 +77,11 @@ int main(int argc, char * argv[])
   char *port;
   int baud;
   void *serial;
-  void *task;
+  void *read_task;
+  void *write_task;
 
   if (argc < 3) {
-    fprintf(stderr, "syntax: serial <port> <baud>\n");
+    fprintf(stderr, "syntax: smartmotor_md <port> <baud>\n");
     return 1;
   }
 
@@ -50,6 +93,9 @@ int main(int argc, char * argv[])
     return 1;
   }
 
+  ulapi_mutex_init(&mutex, 1);
+  ulapi_mutex_give(&mutex);
+  
   if (NULL == (serial = ulapi_serial_new())) {
     fprintf(stderr, "Can't get a serial object\n");
     return 1;
@@ -65,16 +111,19 @@ int main(int argc, char * argv[])
     return 1;
   }
 
-  task = ulapi_task_new();
-  ulapi_task_start(task, task_code, serial, ulapi_prio_lowest(), 0);
+  read_task = ulapi_task_new();
+  ulapi_task_start(read_task, read_code, serial, ulapi_prio_lowest(), 0);
+
+  write_task = ulapi_task_new();
+  ulapi_task_start(write_task, write_code, serial, ulapi_prio_lowest(), 0);
 
   while (! feof(stdin)) {
-	int retval;
     printf("> ");
     fflush(stdout);
     if (NULL == fgets(buffer, BUFFERLEN, stdin)) break;
-    retval = ulapi_serial_write(serial, buffer, strlen(buffer));
-	printf("return: %d\n", retval);
+    ulapi_mutex_take(&mutex);
+    ulapi_serial_write(serial, buffer, strlen(buffer));
+    ulapi_mutex_give(&mutex);
   }
 
   return 0;
